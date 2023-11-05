fd49b13938e4d4fe543dff7499aeee6bb33539ff
RickFerry <rick_ferry1@hotmail.com>
2023-11-05 12:03:06 -0300

    Logout implementado.

---
+++ api/src/main/java/br/com/ferrymoney/api/resource/TokenResource.java
@@ -0,0 +1,26 @@
+package br.com.ferrymoney.api.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/tokens")
+public class TokenResource {
+    @DeleteMapping("/revoke")
+    public ResponseEntity<Void> revoke(HttpServletRequest request, HttpServletResponse response) {
+        Cookie cookie = new Cookie("refreshToken", null);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false);
+        cookie.setPath(request.getContextPath() + "/oauth/token");
+        cookie.setMaxAge(0);
+
+        response.addCookie(cookie);
+        return ResponseEntity.noContent().build();
+    }
+}
