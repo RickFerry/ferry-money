27251c899bb3dc4032f05ca2884402360d4f0031
RickFerry <rick_ferry1@hotmail.com>
2023-11-14 16:01:46 -0300

    Acionando calendario no lançamento-cadastro

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 import { InputTextModule } from "primeng/components/inputtext/inputtext";
 import { ButtonModule } from "primeng/components/button/button";
 import { DataTableModule } from "primeng/components/datatable/datatable";
 import { TooltipModule } from "primeng/components/tooltip/tooltip";
 import { InputTextareaModule } from "primeng/components/inputtextarea/inputtextarea";
+import { CalendarModule } from "primeng/components/calendar/calendar";
 
 import { AppComponent } from "./app.component";
 import { LancamentosPesquisaComponent } from "./lancamentos-pesquisa/lancamentos-pesquisa.component";
@@ -22,11 +24,13 @@ import { LancamentoCadastroComponent } from "./lancamento-cadastro/lancamento-ca
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     InputTextModule,
     ButtonModule,
     DataTableModule,
     TooltipModule,
     InputTextareaModule,
+    CalendarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
