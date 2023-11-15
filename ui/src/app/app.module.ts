5b95647bd7deda85810a6f87253261c98d89c804
RickFerry <rick_ferry1@hotmail.com>
2023-11-15 16:12:20 -0300

    Adicionando componentes SelectButton e DropDown

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
+
 import { InputTextModule } from "primeng/components/inputtext/inputtext";
 import { ButtonModule } from "primeng/components/button/button";
 import { DataTableModule } from "primeng/components/datatable/datatable";
 import { TooltipModule } from "primeng/components/tooltip/tooltip";
 import { InputTextareaModule } from "primeng/components/inputtextarea/inputtextarea";
 import { CalendarModule } from "primeng/components/calendar/calendar";
+import { SelectButtonModule } from "primeng/components/selectbutton/selectbutton";
+import { DropdownModule } from "primeng/components/dropdown/dropdown";
 
 import { AppComponent } from "./app.component";
 import { LancamentosPesquisaComponent } from "./lancamentos-pesquisa/lancamentos-pesquisa.component";
@@ -31,6 +34,8 @@ import { LancamentoCadastroComponent } from "./lancamento-cadastro/lancamento-ca
     TooltipModule,
     InputTextareaModule,
     CalendarModule,
+    SelectButtonModule,
+    DropdownModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- ui/src/app/lancamento-cadastro/lancamento-cadastro.component.ts
+++ ui/src/app/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-lancamento-cadastro',
-  templateUrl: './lancamento-cadastro.component.html',
-  styleUrls: ['./lancamento-cadastro.component.css']
+  selector: "app-lancamento-cadastro",
+  templateUrl: "./lancamento-cadastro.component.html",
+  styleUrls: ["./lancamento-cadastro.component.css"],
 })
-export class LancamentoCadastroComponent implements OnInit {
+export class LancamentoCadastroComponent {
+  tipos = [
+    { label: "Receita", value: "RECEITA" },
+    { label: "Despesa", value: "DESPESA" },
+  ];
 
-  constructor() { }
+  categorias = [
+    { label: "Alimentação", value: 1 },
+    { label: "Transporte", value: 2 },
+  ];
 
-  ngOnInit() {
-  }
-
+  pessoas = [
+    { label: "João da Silva", value: 4 },
+    { label: "Sebastião Souza", value: 9 },
+    { label: "Maria Abadia", value: 3 },
+  ];
 }
