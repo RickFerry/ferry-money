34d8907e35e254c82444301d182933ef69399769
RickFerry <rick_ferry1@hotmail.com>
2023-11-23 17:25:35 -0300

    Paginação lazy em pesquisa de lançamentos

--- ui/src/app/lancamentos/lancamento.service.ts
+++ ui/src/app/lancamentos/lancamento.service.ts
@@ -2,10 +2,12 @@ import { Injectable } from "@angular/core";
 import { Headers, Http, URLSearchParams } from "@angular/http";
 import * as moment from "moment";
 
-export interface LancamentoFiltro {
+export class LancamentoFiltro {
   descricao: string;
   dataVencimentoInicio: Date;
   dataVencimentoFim: Date;
+  pagina = 0;
+  itensPorPagina = 5;
 }
 
 @Injectable()
@@ -22,6 +24,10 @@ export class LancamentoService {
       "Authorization",
       "Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg=="
     );
+
+    params.set("page", filtro.pagina.toString());
+    params.set("size", filtro.itensPorPagina.toString());
+
     if (filtro.descricao) {
       params.set("descricao", filtro.descricao);
     }
@@ -40,6 +46,15 @@ export class LancamentoService {
     return this.http
       .get(`${this.lancamentoUrl}?resumo`, { headers, search: params })
       .toPromise()
-      .then((resp) => resp.json().content);
+      .then((resp) => {
+        const respJson = resp.json();
+        const lancamentos = respJson.content;
+
+        const resultado = {
+          lancamentos,
+          total: respJson.totalElements,
+        };
+        return resultado;
+      });
   }
 }
--- ui/src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ ui/src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -1,29 +1,31 @@
 import { Component, OnInit } from "@angular/core";
 import { LancamentoFiltro, LancamentoService } from "../lancamento.service";
+import { LazyLoadEvent } from "primeng/components/common/lazyloadevent";
 
 @Component({
   selector: "app-lancamentos-pesquisa",
   templateUrl: "./lancamentos-pesquisa.component.html",
   styleUrls: ["./lancamentos-pesquisa.component.css"],
 })
-export class LancamentosPesquisaComponent implements OnInit {
-  descricao: string;
-  dataVencimentoInicio: Date;
-  dataVencimentoFim: Date;
+export class LancamentosPesquisaComponent {
+
+  totalRegistros = 0;
+  filtro = new LancamentoFiltro();
   lancamentos = [];
 
   constructor(private service: LancamentoService) {}
 
-  ngOnInit(): void {
-    this.pesquisar();
+  pesquisar(pagina = 0) {
+    this.filtro.pagina = pagina;
+
+    this.service.pesquisar(this.filtro).then((resultado) => {
+      this.totalRegistros = resultado.total;
+      this.lancamentos = resultado.lancamentos;
+    });
   }
 
-  pesquisar() {
-    const filtro: LancamentoFiltro = {
-      descricao: this.descricao,
-      dataVencimentoInicio: this.dataVencimentoInicio,
-      dataVencimentoFim: this.dataVencimentoFim,
-    };
-    this.service.pesquisar(filtro).then((lanc) => (this.lancamentos = lanc));
+  aoMudarPagina(event: LazyLoadEvent) {
+    const pagina = event.first / event.rows;
+    this.pesquisar(pagina);
   }
 }
