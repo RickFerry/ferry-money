a20a6a315608c49dd984e29827b3e76037c35c35
RickFerry <rick_ferry1@hotmail.com>
2023-11-25 17:25:24 -0300

    Trazendo do backend lista de categorias

--- ui/src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
+++ ui/src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -1,24 +1,36 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { CategoriaService } from "app/categorias/categoria.service";
+import { ErrorHandlerService } from "app/core/error-handler.service";
 
 @Component({
   selector: "app-lancamento-cadastro",
   templateUrl: "./lancamento-cadastro.component.html",
   styleUrls: ["./lancamento-cadastro.component.css"],
 })
-export class LancamentoCadastroComponent {
+export class LancamentoCadastroComponent implements OnInit {
+  categorias = [];
   tipos = [
     { label: "Receita", value: "RECEITA" },
     { label: "Despesa", value: "DESPESA" },
   ];
-
-  categorias = [
-    { label: "Alimentação", value: 1 },
-    { label: "Transporte", value: 2 },
-  ];
-
   pessoas = [
     { label: "João da Silva", value: 4 },
     { label: "Sebastião Souza", value: 9 },
     { label: "Maria Abadia", value: 3 },
   ];
+
+  constructor(
+    private service: CategoriaService,
+    private handle: ErrorHandlerService
+  ) {}
+
+  ngOnInit() {
+    this.carregarCategorias();
+  }
+
+  carregarCategorias() {
+    this.service.listarTodas().then((categorias) => {
+      this.categorias = categorias.map((c) => ({ label: c.nome, value: c.id }));
+    });
+  }
 }
